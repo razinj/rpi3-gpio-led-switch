e935037b68c54388757fbefc9f4e1f1e48e8ace0
razinj <nizarjailane@gmail.com>
2019-03-28 04:04:54 +0100

    added post route

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    // res.status(200).json({ message: 'Welcome!' });
     res.render('home', { 
         title: 'GPIO LED Switch',
         ledStatus: 'OFF'
     });
+});
+
+router.post('/ops/:action', (req, res) => {
+    let action = req.params.action;
+    if(action === 'on') {
+        
+    }
+    // res.status(200).redirect('/')
 });
 
 module.exports = router;
