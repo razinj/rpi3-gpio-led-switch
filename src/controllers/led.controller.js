ff69ff9b52261ee74689a94f6ef57b7a306e4f86
razinj <nizarjailane@gmail.com>
2019-03-28 04:08:30 +0100

    added led controller

---
+++ src/controllers/led.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(4, 'out');
+
+const turnOn = () => {
+    led.write(1, error => {
+        if(error) throw error;
+    });
+}
+const turnOff = () => {
+    led.write(0, error => {
+        if(error) throw error;
+    });
+}
